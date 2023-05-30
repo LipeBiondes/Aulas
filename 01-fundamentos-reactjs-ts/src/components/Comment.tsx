400b5a4616871419a57328b9e59e46bda920300f
Lipe biondes <92653779+LipeBiondes@users.noreply.github.com>
2023-05-30 20:48:26 -0300

    Adicionando o TypeScript na nossa aplicação

---
+++ 01-fundamentos-reactjs-ts/src/components/Comment.tsx
@@ -0,0 +1,55 @@
+import { ThumbsUp, Trash } from '@phosphor-icons/react'
+import styles from './Comment.module.css'
+import { Avatar } from './Avatar'
+import { useState } from 'react'
+
+interface CommentProps {
+  content: string
+  onDeleteComment: (comment: string) => void
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
+  const [likeCount, setLikeCount] = useState(0)
+
+  function handleLikeComment() {
+    setLikeCount(state => {
+      return state + 1
+    })
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/lipeBiondes.png"
+        alt=""
+      />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Alefe Filipe</strong>
+              <time title="25 de maio as 20:57h" dateTime="2023-05-24 20:56:02">
+                cerca de 1 hora atrás
+              </time>
+            </div>
+            <button onClick={handleDeleteComment} title="Deletar Comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{content}</p>
+        </div>
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
