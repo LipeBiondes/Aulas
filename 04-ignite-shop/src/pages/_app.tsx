cc71c8993e21dc879d734da13ce1e9f699606cf0
Lipe biondes <92653779+LipeBiondes@users.noreply.github.com>
2023-06-27 18:30:56 -0300

    Criando a Página Home

--- 04-ignite-shop/src/pages/_app.tsx
+++ 04-ignite-shop/src/pages/_app.tsx
@@ -1,3 +1,18 @@
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+
+import logoImg from '../assets/logo.svg'
+import Image from 'next/image'
+import { Container, Header } from '../styles/pages/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  globalStyles()
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
